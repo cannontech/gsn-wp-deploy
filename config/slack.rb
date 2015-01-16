adca51cbe43abe52c4dc4fcdbe4ca52ef4f4ad7b
root <eric@bagofsmashedassholes.com>
2015-01-16 15:39:58 -0600

    updated config

--- config/slack.rb
+++ config/slack.rb
@@ -3,9 +3,11 @@
 ############################################
 
 # Required
-set :slack_subdomain, 'example' # if your Slack subdomain is example.slack.com
-set :slack_token, 'slack_token' # https://my.slack.com/services/new/incoming-webhook
+set :slack_subdomain, 'gsndev' # if your Slack subdomain is example.slack.com
+#set :slack_token, 'slack_token' # https://my.slack.com/services/new/incoming-webhook
 
 # Optional
 set :slack_channel, '#general'
-set :slack_emoji, ':rocket:'
+set :slack_emoji, ':package:'
+
+set :slack_url, 'https://hooks.slack.com/services/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'
